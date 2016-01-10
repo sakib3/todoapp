6cb4eca28d5380f9e022ca35ac1c89ea38a692dd
sakib3 <sakib3@gmail.com>
2016-01-11 00:08:11 +0100

    taskList add and delete method is developed

--- app/controllers.js
+++ app/controllers.js
@@ -3,4 +3,19 @@
 angular.module('todoApp.controller', [])
 	   .controller('todoController',["$scope", function($scope){
 	   		$scope.newTask = "";
+
+	   		$scope.taskList = [
+	   			{description: "Buy airplane tickets", done: false},
+	   			{description: "Make hotel reservation", done: false},
+	   			{description: "Chill", done: false}
+	   		];
+
+	   		$scope.addTodo = function(){
+	   			$scope.taskList.push({description: $scope.newTask, done: false});
+	   			$scope.newTask = "";
+	   		}
+
+	   		$scope.deleteTodo = function($index){
+	   			$scope.taskList.splice($index,1);
+	   		}
 	   }]);
