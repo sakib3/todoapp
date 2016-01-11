72d6603e6b6548f77df0c596dae0210174c0b799
sakib3 <sakib3@gmail.com>
2016-01-11 19:30:22 +0100

    todo-count status details

--- app/controllers.js
+++ app/controllers.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('todoApp.controller', [])
-	   .controller('todoController',["$scope", function($scope){
+	   .controller('todoController',["$scope", "$filter", function($scope, $filter){
 	   		$scope.newTask = "";
+	   		$scope.pendingCount = 3;
 
 	   		$scope.taskList = [
 	   			{description: "Buy airplane tickets", done: false},
@@ -17,5 +18,13 @@ angular.module('todoApp.controller', [])
 
 	   		$scope.deleteTodo = function($index){
 	   			$scope.taskList.splice($index,1);
+	   		}
+
+	   		$scope.$watch('taskList', function(){
+	   			$scope.pendingCount = $filter('filter')($scope.taskList, {done: false}).length;
+	   		},true)
+
+	   		$scope.clearCompleted = function(){
+	   			$scope.taskList = $filter('filter')($scope.taskList, {done: false});
 	   		}
 	   }]);
